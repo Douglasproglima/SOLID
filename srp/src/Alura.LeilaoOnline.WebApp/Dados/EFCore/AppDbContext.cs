a9db373ddb0c3ebfcd52641dc6e45fe12324c977
Douglas Lima <Douglasproglima@gmail.com>
2022-01-19 23:33:25 -0300

    Tests connectionString Sql Server With EFCore

--- srp/src/Alura.LeilaoOnline.WebApp/Dados/EFCore/AppDbContext.cs
+++ srp/src/Alura.LeilaoOnline.WebApp/Dados/EFCore/AppDbContext.cs
@@ -5,12 +5,14 @@ namespace Alura.LeilaoOnline.WebApp.Dados.EFCore
 {
     public class AppDbContext : DbContext
     {
+        //private string connectionSqlServer = "Server=(localdb)\\MSSQLLocalDB;Database=AluraLeiloesDB;Trusted_Connection=true";
+        private string connectionSqlServer = "Server=.\\SQLEXPRESS;Database=AluraLeiloesDB;User Id=sa;password=root;Trusted_Connection=true;MultipleActiveResultSets=true;Integrated Security=False";
         public DbSet<Leilao> Leiloes { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=AluraLeiloesDB;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(connectionSqlServer);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
