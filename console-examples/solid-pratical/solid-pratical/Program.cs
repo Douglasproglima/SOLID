12087985abd954a35009e0191a1ea9966c460c7a
Douglas Lima <Douglasproglima@gmail.com>
2022-01-24 22:12:14 -0300

    start repository pratical solid

---
+++ console-examples/solid-pratical/solid-pratical/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solid_pratical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
